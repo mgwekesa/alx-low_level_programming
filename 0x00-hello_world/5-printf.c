669a350e418888bbfb63affe71d33d0e6afe19fb
mgwekesa <mgwekesa@gmail.com>
2022-03-10 13:25:25 +0300

    a C program that prints exactly 'Programming is like building a multilingual puzzle', followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+* prints 'with proper grammar, but the outcome is a piece of art,'
+*/
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return - Returns 0 (success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
