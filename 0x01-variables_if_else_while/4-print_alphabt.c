30983b5541d40cc93b98c8e81a6115706d7b7204
mgwekesa <mgwekesa@gmail.com>
2022-03-11 11:47:07 +0300

    prints the alphabet in lowercase, except 'q' and 'e', followed by a new line using for..loop and if..else statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * Printing the letters of alphabet using putchar and while loop
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
