b4b3b1aba609032981320d323ceff55f92af3669
mgwekesa <mgwekesa@gmail.com>
2022-03-15 09:50:05 +0300

    prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/*
+ * prints _putchar, followed by a new line
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("_printf\n");
+	return (0);
+}
