2f96ab763653957cf83ad7ff0b44907b51415f39
mgwekesa <mgwekesa@gmail.com>
2022-03-15 17:06:52 +0300

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+/*
+ * the main function of printing every minute of the day of jack bauer
+ */
+
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int val);
 int print_last_digit(int val);
+void jack_bauer(void);
