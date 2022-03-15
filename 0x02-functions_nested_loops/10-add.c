66b8a25ab40653b6dffead28bb263bad62683ae1
mgwekesa <mgwekesa@gmail.com>
2022-03-15 18:36:50 +0300

    a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * this function takes two numbers, returns the sum
+ */
+
+#include "main.h"
+
+/**
+ * add - function that does the addition
+ * Return: returns the result
+ * @num1: the first parameter
+ * @num2: the second parameter
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int val);
 int print_last_digit(int val);
 void jack_bauer(void);
 void times_table(void);
+int add(int num1, int num2);
