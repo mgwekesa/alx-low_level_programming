230c4dbbbc08e945514a985749b561547b609e47
mgwekesa <mgwekesa@gmail.com>
2022-03-17 19:23:26 +0300

    prints an integer using recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,37 @@
 #include "main.h"
 
 /**
- * print_number - prints the given number
- * @n: the parameter the print_number takes
- */
-
-/*
- * the concept of recursion is tested in this exercise
+ * print_number - a function that prints the integer
+ * @n: is the parameter that print_number takes
  */
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		print_integer(n * -1);
 	}
+	else
+		print_integer(n);
+}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+/**
+ * print_integer - it prints n
+ * @m: the parameter the print_number takes
+ */
+
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+	{
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
+	}
 }
