7e8848633a4d32384012ef32a41e6dc9d0f2f3f3
mgwekesa <mgwekesa@gmail.com>
2022-03-17 13:26:14 +0300

    prints a number if it is not a multiple of 3 and 5, prints Fizz when multiple of 3, Buzz when multiple of 5 and FizzBuzz when multiple of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * fizzBuzz - prints Fizz for multiples of three
+ *          - prints Buzz for multiples of five
+ *          - prints FizzBuzz for multiples of three and five
+ */
+
+void fizzBuzz(void)
+{
+	int n = 1;
+
+	for (; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+
+		else
+			printf("%d", n);
+
+		if (n == 100)
+			continue;
+		printf(" ");
+	}
+}
+
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	fizzBuzz();
+	return (0);
+}
