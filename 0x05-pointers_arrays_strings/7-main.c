495c833d02d0b953ca1d309c8ff0eb04caeee748
mgwekesa <mgwekesa@gmail.com>
2022-03-22 01:18:56 +0300

    print half of a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test code
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: the parameter of the string we'd use
+ */
+
+void puts_half(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (str[count] != '\0')
+		count++;
+
+	if (count % 2 == 0)
+	{
+		for (i = (count / 2); i < count; i++)
+			_putchar(str[i]);
+	}
+
+	else
+	{
+		for (i = (count - 1) / 2; i < count; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
