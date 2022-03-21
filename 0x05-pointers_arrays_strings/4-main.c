412f55c80d94fff9bad1b4c4e7aa79f9bfd5982b
mgwekesa <mgwekesa@gmail.com>
2022-03-21 23:25:50 +0300

    printing the string in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - determines the length of the string
+ * @s: is the parameter of the string
+ * Return: returns the counter
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+	while (s[counter] != '\0')
+		counter++;
+	return (counter);
+}
+
+/**
+ * print_rev - prints the string in reverse
+ * @s: is the string parameter
+ */
+
+void print_rev(char *s)
+{
+	int count = _strlen(s);
+	while (count >= 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar('\n');
+}
