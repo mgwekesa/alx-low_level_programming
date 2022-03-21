b2e52940add30acb1826bdf28bd2653669400a8e
mgwekesa <mgwekesa@gmail.com>
2022-03-21 22:08:53 +0300

    create a 0x05-pointers_arrays_strings directory, the README.md file and using the pointers to update the value of the location to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the parameter to 98
+ * @n: pointer to n used as a parameter
+ * Return: returns a null value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
