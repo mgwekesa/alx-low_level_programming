e738119142c790a27a7628cc89bff891de296338
mgwekesa <mgwekesa@gmail.com>
2022-03-22 03:06:24 +0300

    copies the string pointed to by src, including the terminating null byte to the buffer pointed to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
-		if (dest[i] == '\0')
-			continue;
 		i++;
 	}
 	return (dest);
