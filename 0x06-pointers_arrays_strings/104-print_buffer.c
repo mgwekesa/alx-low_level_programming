911210cf7d3d911336723f287f8aa6e24a62b199
mgwekesa <mgwekesa@gmail.com>
2022-03-25 02:34:49 +0300

    prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -4,7 +4,7 @@
 /**
  * print_buffer - print the size of the content of the buffer pointed
  *                by b, 10 bytes per line. Position of first byte is 0.
- *                each line has hex content, separated by a space
+ *                Each line has hex content, separated by a space
  *                if size <= 0, output '\n'
  * @b: buffer to be printed
  * @size: number of bytes to be printed from the buffer.
@@ -12,44 +12,41 @@
 
 void print_buffer(char *b, int size)
 {
-        int byte;
+	int byte;
 	int i;
 
-        for (byte = 0; byte < size; byte += 10)
-        {
-                printf("%08x: ", byte);
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + i + byte));
 
-                for (i = 0; i < 10; i++)
-                {
-                        if ((i + byte) >= size)
-                                printf("  ");
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
 
-                        else
-                                printf("%02x", *(b + i + byte));
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				break;
 
-                        if ((i % 2) != 0 && i != 0)
-                                printf(" ");
-                }
+			else if (*(b + i + byte) >= 31 &&
+				 *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
+			else
+				printf(".");
+		}
 
-                for (i = 0; i < 10; i++)
-                {
-                        if ((i + byte) >= size)
-                                break;
+		if (byte >= size)
+			continue;
 
-                        else if (*(b + i + byte) >= 31 &&
-                                 *(b + i + byte) <= 126)
-                                printf("%c", *(b + i + byte));
+		printf("\n");
+	}
 
-                        else
-                                printf(".");
-                }
-
-                if (byte >= size)
-                        continue;
-
-                printf("\n");
-        }
-
-        if (size <= 0)
-                printf("\n");
+	if (size <= 0)
+		printf("\n");
 }
