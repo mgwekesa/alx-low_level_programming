05733ed5433d80f554b3b8ce80b9e11dd3aa6b2f
mgwekesa <mgwekesa@gmail.com>
2022-03-25 00:46:54 +0300

    adding one line to the given code, so that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+ * base10 - raised to base 10 function
+ * @n: the exponent
+ * Return: returns 10 raised to the exponent
+ */
+
+int base10(int n)
+{
+	int base = 10;
+
+	for (; n > 0; n--)
+		base *= 10;
+	return (base);
+}
 
 /**
  * print_number - prints the number with _putchar
@@ -7,16 +21,26 @@
 
 void print_number(int n)
 {
+	int raisedTo = base10(8);
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
 
-	if ((n / 10) > 0)
-	{
-		print_number(n / 10);
-	}
+	if (n == 0)
+		_putchar('0');
 
-	_putchar((n % 10) + '0');
+	else
+	{
+		while (n / raisedTo == 0)
+			raisedTo /= 10;
+		while (raisedTo >= 1)
+		{
+			_putchar((n / raisedTo) + '0');
+			n %= raisedTo;
+			raisedTo /= 10;
+		}
+	}
 }
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * adds_strings - function performs the operation of adding numbers in string
+ * @n1: string with 1st number
+ * @n2: string with 2nd number
+ * @r: buffer st
