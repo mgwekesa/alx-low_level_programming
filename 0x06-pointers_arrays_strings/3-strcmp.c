4a8bb5023dc6367b6ef5aa1bacd498b1a69d00fd
mgwekesa <mgwekesa@gmail.com>
2022-03-24 18:06:19 +0300

    performs comparison between strings. Returns 15 is the first non-matching character in s1 is greater in ASCII than in s2. Returns -15 if it is the other way round. If the strings matches, it returns 0.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,26 +4,17 @@
  * _strcmp - compares the string the way strcmp does it
  * @s1: the first string parameter
  * @s2: the second string parameter
- * Return: returns 0 when s1 and s2 is the same,
- *         returns 15 when first non-matching character in s1 is
- *         greater(in ASCII) than s2
- *         returns -15 when first non_matching character in s1 is
- *         less(in ASCII) than s2
- *         returns -1 when the function fail to run
+ * Return: if s1 == s2, 0
+ *         if s1 < s2, the negative difference of the first unmatched character
+ *         if s1 > s2, the positive difference of the first unmatched character
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i++] && s2[i++])
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] == s2[i])
-			return (0);
-		else if (s1[i] > s2[i])
-			return (15);
-		else if (s1[i] < s2[i])
-			return (-15);
+		s1++;
+		s2++;
 	}
-	return (-1);
+	return (*s1 - *s2);
 }
