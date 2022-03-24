3442cb442d665029849c2b79115f3b82cf7bd398
mgwekesa <mgwekesa@gmail.com>
2022-03-25 02:25:06 +0300

    prints a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - print the size of the content of the buffer pointed
+ *                by b, 10 bytes per line. Position of first byte is 0.
+ *                each line has hex content, separated by a space
+ *                if size <= 0, output '\n'
+ * @b: buffer to be printed
+ * @size: number of bytes to be printed from the buffer.
+ */
+
+void print_buffer(char *b, int size)
+{
+        int byte;
+	int i;
+
+        for (byte = 0; byte < size; byte += 10)
+        {
+                printf("%08x: ", byte);
+
+                for (i = 0; i < 10; i++)
+                {
+                        if ((i + byte) >= size)
+                                printf("  ");
+
+                        else
+                                printf("%02x", *(b + i + byte));
+
+                        if ((i % 2) != 0 && i != 0)
+                                printf(" ");
+                }
+
+                for (i = 0; i < 10; i++)
+                {
+                        if ((i + byte) >= size)
+                                break;
+
+                        else if (*(b + i + byte) >= 31 &&
+                                 *(b + i + byte) <= 126)
+                                printf("%c", *(b + i + byte));
+
+                        else
+                                printf(".");
+                }
+
+                if (byte >= size)
+                        continue;
+
+                printf("\n");
+        }
+
+        if (size <= 0)
+                printf("\n");
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ void print_number(int n);
 int base10(int n);
 char *adds_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
