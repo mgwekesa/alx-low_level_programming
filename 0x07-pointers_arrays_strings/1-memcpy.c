0dba2eb6d4a97ad2cade927c84f061187aa13d37
mgwekesa <mgwekesa@gmail.com>
2022-03-28 17:01:33 +0300

    copies the memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: pointer to the destination memory area
  * @src: pointer to the source memory area
  * @n: the number of bytes to be copied
+ * Return: returns the pointer to destination memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
