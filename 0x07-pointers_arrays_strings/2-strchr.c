5afd5a0762bcae670b74fd4df049ff85b56f9d3a
mgwekesa <mgwekesa@gmail.com>
2022-03-28 17:39:58 +0300

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,10 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
-		i++;
 	}
 
 	return ('\0'); /* returns NULL if there is no match */
