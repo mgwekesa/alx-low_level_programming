38b19cd02e7328efb189357d71e2b4556db6715f
mgwekesa <mgwekesa@gmail.com>
2022-04-02 01:29:30 +0300

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry ppint for the program. Prints the program name,
+ *        followed by a new line
+ * @argc: the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: returns 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
