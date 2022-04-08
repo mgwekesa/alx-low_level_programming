1216c53041de772246eb9742583620504abcac72
mgwekesa <mgwekesa@gmail.com>
2022-04-08 12:30:38 +0300

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,12 +36,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ('\0');
 
 	len = 0;
-	i = 0;
-	while (s1[i])
-	{
+	for (i = 0; s1[i]; i++)
 		nconcat[len++] = s1[i];
-		i++;
-	}
 
 	for (i = 0; s2[i] && i < n; i++)
 		nconcat[len++] = s2[i];
