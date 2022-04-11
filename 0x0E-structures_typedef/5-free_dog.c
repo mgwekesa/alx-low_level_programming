3a4f3444a727d9e678e37eb21c7e5cee3d780a6b
mgwekesa <mgwekesa@gmail.com>
2022-04-12 01:23:33 +0300

    frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * free_dog - frees a dog structure
+  * @d: pointer to the struct to be freed
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d -> name != NULL)
+		free(d->name);
+
+	if (d-owner != NULL)
+		free(d->owner);
+
+	free(d);
+}
