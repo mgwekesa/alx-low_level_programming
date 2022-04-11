d6dc18acc2fa44b4514765c717539168ac65e5ee
mgwekesa <mgwekesa@gmail.com>
2022-04-12 00:00:09 +0300

    define a new type struct dog with name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,20 @@
+#ifndef _DOH_H_
+#define _DOG_H_
+/** struct dog - the structure of a dog type
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+  */
+
 struct dog {
 	char * name;
 	float age;
 	char * owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
