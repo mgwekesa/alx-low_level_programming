1ef57ca144fe288cf37ee30a651ce2acfb1e63bf
mgwekesa <mgwekesa@gmail.com>
2022-04-23 00:55:02 +0300

    uses constructor to print a statement before the main function executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * main_constructor - prints a message before main runs
+  */
+
+void main_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
